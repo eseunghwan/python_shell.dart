3df9db72182da22d2077805f21fb63fb1d2e7e1f
guyluz11 <guyluz11@gmail.com>
2022-12-06 13:22:12 +0200

    Updated python default version to 3.10.6

--- lib/src/config.dart
+++ lib/src/config.dart
@@ -1,40 +1,38 @@
-
 import "dart:io";
+
 import "package:path/path.dart" as path;
 
-
-String appDir = path.join(Platform.environment[Platform.isWindows ? "USERPROFILE" : "HOME"]!, ".python_shell.dart");
+String appDir = path.join(
+    Platform.environment[Platform.isWindows ? "USERPROFILE" : "HOME"]!,
+    ".python_shell.dart");
 String tempDir = path.join(appDir, "temp");
 String instanceDir = path.join(appDir, "instances");
 
-String defaultPythonVersion = "3.9.13";
+String defaultPythonVersion = "3.10.6";
 String defaultPythonPath = "python3";
 
 String checkPythonVersion(String rawPythonVersion) {
-    String realPythonVersion = "3.9.13";
+  String realPythonVersion = "3.10.6";
 
-    var versions = rawPythonVersion.split(".");
-    if (versions.length == 3) {
-        if (rawPythonVersion.endsWith(".")) {
-            if (versions.last != "") {
-                versions.removeLast();
-                realPythonVersion = versions.join(".");
-            }
-        }
-        else {
-            realPythonVersion = rawPythonVersion;
-        }
+  var versions = rawPythonVersion.split(".");
+  if (versions.length == 3) {
+    if (rawPythonVersion.endsWith(".")) {
+      if (versions.last != "") {
+        versions.removeLast();
+        realPythonVersion = versions.join(".");
+      }
+    } else {
+      realPythonVersion = rawPythonVersion;
     }
-    else if (versions.length == 2) {
-        if (rawPythonVersion.endsWith(".")) {
-            if (versions.last != "") {
-                realPythonVersion = "${versions.join(".")}.0";
-            }
-        }
-        else {
-            realPythonVersion = "$rawPythonVersion.0";
-        }
+  } else if (versions.length == 2) {
+    if (rawPythonVersion.endsWith(".")) {
+      if (versions.last != "") {
+        realPythonVersion = "${versions.join(".")}.0";
+      }
+    } else {
+      realPythonVersion = "$rawPythonVersion.0";
     }
+  }
 
-    return realPythonVersion;
+  return realPythonVersion;
 }
